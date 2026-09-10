041e246550c5324a039e15753483a6c20291cd97
B. Sakthi Priyan <sec23cs225@sairamtap.edu.in>
2026-09-10 20:53:55 +0530

    Add production error logging

--- config/settings.py
+++ config/settings.py
@@ -431,3 +431,26 @@ DEFAULT_FROM_EMAIL = os.getenv(
 DEFAULT_AUTO_FIELD = (
     "django.db.models.BigAutoField"
 )
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
